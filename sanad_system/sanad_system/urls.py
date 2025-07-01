edbc0d73b8e46aa61d0bf3c64a4f319b9d413f7c
maysaraadmin <maysara.mubarak.mohamed@gmail.com>
2025-07-01 13:03:58 +0400

    fix

--- sanad_system/sanad_system/urls.py
+++ sanad_system/sanad_system/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hadith_app.urls', namespace='hadith_app')),
+    path('', include('hadith_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
