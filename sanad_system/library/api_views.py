6b8195a8f6714cf1ca2555f56061ff9b7c367954
maysaraadmin <maysara.mubarak.mohamed@gmail.com>
2025-07-07 23:34:11 +0400

    fix

--- sanad_system/library/api_views.py
+++ sanad_system/library/api_views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from ..models import Document, DocumentCategory
+from library.models import Document, DocumentCategory
 
 
 class DocumentSuggestionsAPIView(APIView):
