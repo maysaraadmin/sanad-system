6ba05f2ef30df510f72c9424c12c9da11b1415ea
maysaraadmin <maysara.mubarak.mohamed@gmail.com>
2025-07-11 11:08:53 +0400

    delete library

--- sanad_system/pdf_reader/templatetags/__init__.py
+++
@@ -1,7 +0,0 @@
-# This file makes this directory a Python package
-
-# Import the register function to make it available when the package is imported
-from .pdf_reader_tags import register
-
-# Make the register function available at the package level
-__all__ = ['register']
